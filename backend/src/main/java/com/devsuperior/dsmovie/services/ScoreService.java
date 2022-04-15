b11f47cf91e093669a0c94a9c6cb9d65e77ccc23
Liza Magalhaes <liza.magalhaes@hotmail.com>
2022-04-14 22:30:06 -0300

    Correcao - Save score

--- backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.entities.ScorePK;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,45 +17,49 @@ import com.devsuperior.dsmovie.repositories.UserRepository;
 @Service
 public class ScoreService {
 
-	@Autowired
-	private MovieRepository movieRepository;
+    @Autowired
+    private MovieRepository movieRepository;
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private ScoreRepository scoreRepository;
+    @Autowired
+    private ScoreRepository scoreRepository;
 
-	@Transactional
-	public MovieDTO saveScore(ScoreDTO dto) {
+    @Transactional
+    public MovieDTO saveScore(ScoreDTO dto) {
 
-		User user = userRepository.findByEmail(dto.getEmail());
-		if (user == null) {
-			user = new User();
-			user.setEmail(dto.getEmail());
-			user = userRepository.saveAndFlush(user); 
-	}
-			Movie movie = movieRepository.findById(dto.getMovieId()).get();
+        User user = userRepository.findByEmail(dto.getEmail());
+        if (user == null) {
+            user = new User();
+            user.setEmail(dto.getEmail());
+            user = userRepository.saveAndFlush(user);
+        }
+        Movie movie = movieRepository.findById(dto.getMovieId()).get();
 
-			Score score = new Score();
-			score.SetMovie(movie);
-			score.SetUser(user);
-			score.setValue(dto.getScore());
+        Score score = new Score();
+        score.SetMovie(movie);
+        score.SetUser(user);
+        score.setValue(dto.getScore());
+        ScorePK scorePK = new ScorePK();
+        scorePK.setMovie(movie);
+        scorePK.setUser(user);
+        score.setId(scorePK);
 
-			score = scoreRepository.saveAndFlush(score); 
+        score = scoreRepository.saveAndFlush(score);
 
-			double sum = 0.0; 
-			for(Score s: movie.getScores()) {
-				sum = sum + s.getValue();
-	}
-			double avg = sum / movie.getScores().size(); 
+        double sum = 0.0;
+        for (Score s : movie.getScores()) {
+            sum = sum + s.getValue();
+        }
+        double avg = sum / movie.getScores().size();
 
-			movie.setScore(avg);
-			movie.setCount(movie.getScores().size()); 
+        movie.setScore(avg);
+        movie.setCount(movie.getScores().size());
 
-			movie = movieRepository.save(movie); 
+        movie = movieRepository.save(movie);
 
-			return new MovieDTO(movie); 
+        return new MovieDTO(movie);
 
-	} 
+    }
 } 
